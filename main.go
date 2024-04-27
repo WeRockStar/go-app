b4a540bbbe7a022552287a026f6c2e5e43c251fa
WeRockStar <kotchaphan.m@gmail.com>
2024-04-27 23:09:08 +0700

    add ping endpoint

--- main.go
+++ main.go
@@ -10,5 +10,8 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]string{"message": "Hello, Go Workshop!"})
 	})
+	e.GET("/ping", func(c echo.Context) error {
+		return c.String(http.StatusOK, "pong")
+	})
 	e.Logger.Fatal(e.Start(":8080"))
 }
